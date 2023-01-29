e1329f08b0c815c3b588d8c30b7a4e5be48d219e
nufke <nufke@barnasconi.net>
2023-01-29 13:24:48 +0100

    add web services for VI controls

--- bin/lox-mqtt-gateway.js
+++ bin/lox-mqtt-gateway.js
@@ -1,9 +1,10 @@
 #!/usr/bin/env node
 
 const lox_mqtt_gateway = require('../lib/index.js');
+const http = require('http');
 
-if (!process.env.NODE_CONFIG_DIR){
-    process.env.NODE_CONFIG_DIR = __dirname+"/../config/";
+if (!process.env.NODE_CONFIG_DIR) {
+    process.env.NODE_CONFIG_DIR = __dirname + "/../config/";
 }
 var config = require("config");
 
@@ -16,14 +17,14 @@ var mqtt_client = lox_mqtt_gateway.mqtt_builder(config.get('mqtt'), app);
 var lox_client = lox_mqtt_gateway.WebSocketAPI(config.get('miniserver'), app);
 var lox_mqtt_adaptor = undefined;
 
-app.on('exit', function(code) {
-    if (lox_mqtt_adaptor){
+app.on('exit', function (code) {
+    if (lox_mqtt_adaptor) {
         lox_mqtt_adaptor.abort();
     }
 });
 
-function _update_event(uuid, value){
-    if (lox_mqtt_adaptor){
+function _update_event(uuid, value) {
+    if (lox_mqtt_adaptor) {
         lox_mqtt_adaptor.set_value_for_uuid(uuid, value);
     }
 };
@@ -31,48 +32,64 @@ function _update_event(uuid, value){
 lox_client.on('update_event_text', _update_event);
 lox_client.on('update_event_value', _update_event);
 
-lox_client.on('get_structure_file', function(data) {
-    if (lox_mqtt_adaptor){
+lox_client.on('get_structure_file', function (data) {
+    if (lox_mqtt_adaptor) {
         lox_mqtt_adaptor.abort();
     }
 
     lox_mqtt_adaptor = new lox_mqtt_gateway.Adaptor(lox_mqtt_gateway.Structure.create_from_json(data,
-        function(value) {
+        function (value) {
             logger.warn("MQTT Structure - invalid type of control", value);
         }
     ));
 
     mqtt_client.subscribe(lox_mqtt_adaptor.get_topics_for_subscription());
 
-    lox_mqtt_adaptor.on('for_mqtt', function(topic, data, retain_){
+    lox_mqtt_adaptor.on('for_mqtt', function (topic, data, retain_) {
         let payload = String(data);
         let options = { retain: retain_ };
-        logger.debug("MQTT Adaptor - for mqtt: ", {topic: topic, data: payload});
+        logger.debug("MQTT Adaptor - for mqtt: ", { topic: topic, data: payload });
         var fixedTopicName = topic.replace("+", "_").replace("#", "_")
         mqtt_client.publish(fixedTopicName, payload, options);
     });
 
     if (config.loxberrypwa.publish_structure)
-      lox_mqtt_adaptor.publish_mqtt_structure();
+        lox_mqtt_adaptor.publish_mqtt_structure();
 });
 
-mqtt_client.on('connect', function(conack){
-    if (!lox_client.is_connected()){
+mqtt_client.on('connect', function (conack) {
+    if (!lox_client.is_connected()) {
         lox_client.connect();
     }
 });
 
-mqtt_client.on('message', function(topic, message, packet) {
-    if (!lox_mqtt_adaptor){
+mqtt_client.on('message', function (topic, message, packet) {
+    if (!lox_mqtt_adaptor) {
         return;
     }
     var action = lox_mqtt_adaptor.get_command_from_topic(topic, message.toString());
 
-    app.logger.debug("MQTT Adaptor - for miniserver: ", {uuidAction: action.uuidAction, command: action.command});
-
-    if (!config.miniserver.readonly){
-        lox_client.send_cmd(action.uuidAction, action.command);
-    }else{
+    if (config.miniserver.readonly) {
         app.logger.debug("MQTT Adaptor - readonly mode");
+        return;
+    }
+
+    if (action.protocol === 'ws') {
+        lox_client.send_cmd(action.uuidAction, action.command);
+        app.logger.debug("MQTT Adaptor - for miniserver: ", { uuidAction: action.uuidAction, command: action.command });
+        return;
+    }
+
+    if (action.protocol === 'http') {
+        var url = 'http://' + config.miniserver.username + ':' + config.miniserver.password +
+            '@' + config.miniserver.host + '/jdev/sps/io/' + action.name + '/' + action.command;
+
+        http.get(url, resp => {
+            app.logger.debug("MQTT Adaptor - for miniserver: ", url);
+        })
+        .on('error', err => {
+            app.logger.error("MQTT Adaptor - HTTP error: " + err.message);
+        })
+        return;
     }
 });
